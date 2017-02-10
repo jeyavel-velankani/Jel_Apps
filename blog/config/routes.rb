d6d3dc7289a5458ee2bd1f63a3f5e0b7474d78fb
Jeyavel.Natesan <jeyavel.natesan@velankani.com>
2017-02-10 16:31:03 +0530

    Added test search using the elasticsearch search enging server and also ref docs

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   
   resources :posts
   resources :authors
+  resources :articles
+  get 'search', to: 'search#search'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205100502) do
+ActiveRecord::Schema.define(version: 20170210102851) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
